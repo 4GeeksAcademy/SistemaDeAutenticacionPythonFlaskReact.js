0a7103903066b272187b7e651bb620dbe3f36dcd
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-05-29 04:23:29 +0000

    avances en el backend

--- src/app.py
+++ src/app.py
@@ -6,10 +6,15 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db,User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 # from models import Person
 
@@ -18,6 +23,9 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT-KEY") #super secret
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -66,6 +74,26 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0  # avoid cache memory
     return response
+
+@app.route("/login",methods=["POST"])
+def login():
+    body=request.get_json(silent=True)
+    if body is None:
+        return jsonify({"msg":"body esta vacio"}),400
+    if "email" not in body:
+        return jsonify({"msg":"el email es requerido"}),400
+    if "password" not in body:
+        return jsonify({"msg":"el password es requerido"}),400
+    user=User.query.filter_by(email=body["email"]).all()
+    if len(user)==0:
+        return jsonify({"msg":"no se encuentra la informacion"}),400
+    if user[0].password != body["password"]:
+        return jsonify({"msg":"no se encuentra la informacion"}),400
+    access_token=create_access_token(identity=user[0].email)
+    return jsonify({"msg":"todo salio bien","access_token":access_token}),200
+
+
+
 
 
 # this only runs if `$ python src/main.py` is executed
